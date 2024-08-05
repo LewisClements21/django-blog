525eb2b6b218c3a3617e88c37273c333d3bef175
Lewis Clements <lewisclements15@icloud.com>
2024-08-05 06:37:57 +0000

    Add cloudinary storage

--- codestar/settings.py
+++ codestar/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -50,7 +51,9 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     'blog',
+    'about',
 ]
 
 SITE_ID = 1
